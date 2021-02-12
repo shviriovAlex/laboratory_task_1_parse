a4f0883f1ccacbc790ab33273e9cdfe838fbff92
Alex <alex.shviriov@gmail.com>
2021-02-13 00:36:33 +0300

    remake of task_1_laboratory

--- parser/task_1_laboratory.py
+++ parser/task_1_laboratory.py
@@ -1,48 +1,72 @@
 import requests
+from requests import exceptions
 from bs4 import BeautifulSoup
 import re
 
 
-class Urls:
-    def __init__(self, main_url):
+class CorrectUrl:
+    def __init__(self, url):
+        self.url = url
+        self.headers = {'user-agent': 'my-app/0.0.1'}
+
+    def check_url(self):
+        try:
+            r = requests.get(self.url, headers=self.headers)
+            r.raise_for_status()
+            print('Successful connection!')
+        except exceptions.HTTPError:
+            print('Something went wrong.Incorrect URL or check your internet connection!')
+            quit('Check if the address is correct')
+        except exceptions.ConnectionError:
+            print(f"Something went wrong. Server Error!\nCan't find {self.url}!")
+            quit('Check if the address is correct')
+        except exceptions.MissingSchema:
+            print(f"Something went wrong. Invalid url!\nTry https://{self.url}!")
+            quit('Check if the address is correct')
+        except exceptions.InvalidSchema:
+            print(f"Something went wrong. Invalid url {self.url}!")
+            quit('Check if the address is correct')
+
+
+class GetPages:
+
+    def __init__(self, main_url, count_pages=None):
         self.main_url = main_url
         self.headers = {'user-agent': 'my-app/0.0.1'}
-        self.count_page = 0
         self.all_urls = []
+        self.count_pages = count_pages
 
-    def correct_url(self):
-        try:
-            r = requests.get(self.main_url, headers=self.headers)
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            print('Incorrect URL', err)
+    def generate_pages(self):
+        CorrectUrl(self.main_url).check_url()
+        all_pages = BeautifulSoup(requests.get(self.main_url, headers=self.headers).content,
+                                  "html.parser").find_all(class_="bloko-button HH-Pager-Control")
+        for page in all_pages:
+            self.count_pages = int(page["data-page"]) + 1
 
-    def count_pages(self):
-        page_number = BeautifulSoup(requests.get(self.main_url, headers=self.headers).content,
-                                    "html.parser").find_all(class_="bloko-button HH-Pager-Control")
-        for page in page_number:
-            self.count_page = int(page["data-page"]) + 1
-
-        for page in range(self.count_page):
-            self.main_url = self.main_url[:-1]
+        for page in range(self.count_pages):
             self.main_url += str(page)
+            CorrectUrl(self.main_url).check_url()
             self.all_urls.append(self.main_url)
+            self.main_url = self.main_url[:-len(str(page))]
 
         return self.all_urls
 
 
 class Parser:
 
-    def __init__(self, words):
+    def __init__(self, words, all_urls):
         self.words = words
         self.headers = {'user-agent': 'my-app/0.0.1'}
         self.all_words = {'python': [], 'flask': [], 'linux': []}
+        self.links = []
+        self.all_urls = all_urls
 
     def page_with_vacancy(self):
-        for url in urls.count_pages():
+        for url in self.all_urls:
             for cv_link in BeautifulSoup(requests.get(url, headers=self.headers).content, 'html.parser').find_all(
                     class_="bloko-link HH-LinkModifier"):
                 cv_page = requests.get(cv_link['href'], headers=self.headers)
+                self.links.append(cv_page)
                 cv_page_soup = BeautifulSoup(cv_page.content, 'html.parser').find_all(
                     class_="bloko-gap bloko-gap_bottom")
 
@@ -58,11 +82,10 @@ class Parser:
                f"Average number Linux, {sum(self.all_words['linux']) / len(self.all_words['linux'])}"
 
 
-if __name__ == '__main__':
-    urls = Urls('https://rabota.by/search/vacancy?L_is_autosearch=false&area=16&clusters=true&enable_snippets=true&text=Python&page=0')
-    pars = Parser(['python', 'flask', 'linux'])
-    urls.correct_url()
-    urls.count_pages()
-    print(pars.page_with_vacancy())
-    print(pars.average_number())
+a = GetPages(
+    f"https://rabota.by/search/vacancy?L_is_autosearch=false&area=16&clusters=true&enable_snippets=true&text=python&page=")
+print(a.generate_pages())
+b = Parser(['python', 'flask', 'linux'], a)
+
+print(b.page_with_vacancy())
 
